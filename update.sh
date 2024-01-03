28a1dae32ff7c83f0cf8e0da13b055ba68974594
andrej-koman <andrej.koman123@gmail.com>
2024-01-03 16:06:47 +0100

    os adaption

--- update.sh
+++ update.sh
@@ -1,1 +1,5 @@
-cp ~/.ideavimrc ./.ideavimrc
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cp ~/.ideavimrc ./.ideavimrc
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo "Windows"
+fi
